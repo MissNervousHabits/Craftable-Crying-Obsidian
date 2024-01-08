4550da82fd40cb7931101c035ab19b38752c7374
BuildTools <unconfigured@null.spigotmc.org>
2024-01-08 16:04:06 -0500

    Initial commit

---
+++ src/main/java/missnervous/craftablecryingobsidian/craftablecryingobsidian.java
@@ -0,0 +1,20 @@
+package missnervous.craftablecryingobsidian;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod("craftablecryingobsidian")
+public class craftablecryingobsidian {
+    public craftablecryingobsidian() {
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+    }
+
+}
